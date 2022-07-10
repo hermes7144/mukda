895bdd60a97ee176a99858168c3f82970d99d15e
hermes7144 <kichancho@gmail.com>
2022-07-10 23:12:16 +0900

    로그아웃 기능 구현

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,113 +1,152 @@
-let postId = 1;
+import Post from '../../models/post'
+import mongoose from 'mongoose'
+import Joi from 'joi'
 
-// posts 배열 초기 데이터
-const posts = [
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id))
   {
-    id: 1,
-    title: '제목',
-    body: '내용'
+    ctx.status = 400;
+    return
   }
-]
-
+  return next();
+}
 /* 포스트 작성
 POST /api/posts
 {title, body}
 */
-export const write = ctx => {
-  // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body }
-  posts.push(post)
-  ctx.body = post;
+export const write = async ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  })
+  const result = schema.validate(ctx.request.body);
+
+
+  if (result.error)
+  {
+    ctx.status = 400
+    ctx.body = result.error;
+    return
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title, body, tags
+  });
+  try
+  {
+    await post.save();
+    ctx.body = post;
+  } catch (e)
+  {
+    ctx.throw(500, e)
+  }
 }
 
 /* 포스트 목록 조회
 GET /api/posts
 */
-export const list = ctx => {
-  ctx.body = posts;
+export const list = async ctx => {
+
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1)
+  {
+    ctx.status = 400;
+    return
+  }
+  try
+  {
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10))
+    ctx.body = posts
+      .map(post => ({
+        ...post,
+        body:
+          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      }))
+  } catch (e)
+  {
+    ctx.throw(500, e)
+  }
+
 }
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-export const read = ctx => {
+export const read = async ctx => {
   const { id } = ctx.params;
-  const post = posts.find(p => p.id.toString() === id);
-  if (!post)
+  try
   {
-    ctx.status = 404
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+    const post = await Post.findById(id).exec();
+    if (!post)
+    {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e)
+  {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
-}
+};
+
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-export const remove = ctx => {
+export const remove = async ctx => {
   const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  // 포스트가 없으면 오류를 반환한ㅂ니다.
-  if (index === -1)
+  try
   {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content
+  } catch (e)
+  {
+    ctx.throw(500, e)
+  }
+}
+
+export const update = async ctx => {
+  const { id } = ctx.params;
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  })
+  const result = schema.validate(ctx.request.body);
+
+  if (result.error)
+  {
+    ctx.status = 400
+    ctx.body = result.error;
+    return
+  }
+
+
+  try
+  {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if (!post)
+    {
+      ctx.status = 404;
+      return
     }
-    return;
-  }
-  posts.splice(index, 1);
-  ctx.status = 204;
-}
-
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{title, body}
-*/
-export const replace = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1)
+    ctx.body = post
+  } catch (e)
   {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
+    ctx.throw(500, e);
   }
-  // 전체 객체를 덮어 씌웁니다.
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index]
-}
-
-
-/* 포스트 수정(특정 필드 변경)
-PATCH /api/posts/:id
-{title, body}
-*/
-export const update = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (index === -1)
-  {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.'
-    };
-    return;
-  }
-  // 전체 객체를 덮어 씌웁니다.
-  // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index]
 }
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,5 +1,36 @@
-// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다
-/* eslint-disable no-global-assign */
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
 
-require = require('esm')(module /*, options*/);
-module.exports = require('./main.js');
+import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware'
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connexted to MongoDB');
+  }).catch(e => {
+    console.error(e)
+  })
+
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.use('/api', api.routes());
+
+app.use(bodyParser());
+app.use(jwtMiddleware)
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods())
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening to port %d`, port)
+})
+
