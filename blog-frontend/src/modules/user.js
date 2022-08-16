345cdf94ada0a441edebfdf2c897052fb89f89bd
hermes7144 <kichancho@gmail.com>
2022-08-16 20:09:45 +0900

    change test

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -7,12 +7,11 @@ import createRequestSaga, {
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
-const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
-  'user/CHECK',
-);
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK');
 const LOGOUT = 'user/LOGOUT';
 
-export const tempSetUser = createAction(TEMP_SET_USER, user => user);
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
 export const logout = createAction(LOGOUT);
 
@@ -62,7 +61,7 @@ export default handleActions(
       user: null,
       checkError: error,
     }),
-    [LOGOUT]: state => ({
+    [LOGOUT]: (state) => ({
       ...state,
       user: null,
     }),
